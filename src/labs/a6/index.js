a7bc548fcb43f00e2c661662d497631126a8ca32
Noah-Mack-01 <noahnewmanmack@gmail.com>
2023-03-10 12:13:27 -0500

    Finish up lab

---
+++ src/labs/a6/index.js
@@ -0,0 +1,19 @@
+import Nav from "../../nav";
+import Styles from "./styles";
+import Classes from "./classes";
+import ConditionalOutput from "./conditional-output";
+import TodoList from "./todo/todo-list"
+
+function Assignment6() {
+ return (
+   <div>
+    <Nav/>
+    <h1>Assignment 6</h1>
+    <ConditionalOutput/>
+    <Classes/>
+    <Styles/>
+    <TodoList/>
+   </div>
+ );
+}
+export default Assignment6;
---
+++ src/labs/index.js
@@ -0,0 +1,11 @@
+import Assignment6 from "./a6";
+import HelloWorld from "./a6/hello-world";
+
+function Labs() {
+ return (
+   <div>
+    <Assignment6/>
+   </div>
+ );
+}
+export default Labs;
