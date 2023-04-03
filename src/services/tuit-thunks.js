5344027cd46a52d1f7c43400d2dfb1cf6c2f6cc2
Noah-Mack-01 <noahnewmanmack@gmail.com>
2023-04-03 16:03:36 -0400

    ugh

--- src/services/tuit-thunks.js
+++ src/services/tuit-thunks.js
@@ -4,9 +4,9 @@ import * as service
   from "./tuits-service"
 
 export const findTuitsThunk = createAsyncThunk(
-  'tuits/findTuits', async () =>
-    await service.findTuits()
-)
+  'tuits/findTuits', async () => await service.findTuits()
+);
+
 export const deleteTuitThunk = createAsyncThunk(
     'tuits/deleteTuit',
     async (tuitId) => {
